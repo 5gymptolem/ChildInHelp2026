26974c57405d8805d072e7dc3ee742b76d862b73
5gymptolem <149423165+5gymptolem@users.noreply.github.com>
2026-06-12 10:30:27 +0300

    Update AppFirebase.kt

--- app/src/main/java/com/childInHelp2026/app/firebase/AppFirebase.kt
+++ app/src/main/java/com/childInHelp2026/app/firebase/AppFirebase.kt
@@ -5,7 +5,7 @@ import com.google.firebase.database.FirebaseDatabase
 
 object AppFirebase {
     const val DATABASE_URL: String =
-        "https://childInHelp2026-default-rtdb.europe-west1.firebasedatabase.app/"
+        "*************************************"
 
     val database: FirebaseDatabase by lazy {
         FirebaseDatabase.getInstance(DATABASE_URL)
